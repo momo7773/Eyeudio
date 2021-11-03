68f8680d9f8b91157e9b91d766962f79a7f9b540
YiqingTao <momo@YiqingTaos-MacBook-Pro.local>
2021-11-03 16:54:52 -0700

    add basic audio, image function

--- Input/img_input.py
+++ Input/img_input.py
@@ -1,7 +1,31 @@
+import numpy as np
+import cv2
+
 class Image_Input(Input):
-    def __init__():
+    def __init__(self):
         pass
 
-    def get_frames():
+    def get_frames(self):
+        cap = cv2.VideoCapture(0)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
 
-    
+            # Our operations on the frame come here
+            if not ret:
+                print('failed to grab frame')
+                break
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+
+            # Display the resulting frame
+            cv2.imshow('frame',gray)
+            cv2.waitKey(3000)
+            if cv2.waitKey(1000) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+
+input = Image_Input()
+input.get_frames()
--- Input/voice_input.py
+++ Input/voice_input.py
@@ -1,5 +1,29 @@
+import sounddevice as sd
+import numpy
+from scipy.io.wavfile import write
+import scipy.io.wavfile as wav
+
+# Sampling frequency
+freq = 44100
+
+# Recording duration
+duration = 5
+
 class Voice_Input(Input):
-    def __init__():
+    def __init__(self):
         pass
-    def get_audio():
-        
+    def get_audio(self):
+        # initialize portaudio
+        print('start recording')
+        recording = sd.rec(int(duration * freq),
+                   samplerate=freq, channels=1)
+
+        # Record audio for the given number of seconds
+        sd.wait()
+        print('end recording')
+        write("recording0.wav", freq, recording)
+        print(recording.shape)
+        return recording
+
+input = Voice_Input()
+input.get_audio()
