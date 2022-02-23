e3e193f3a2ad4020cc0918d3faa29e93fa37bb69
Hoang Nguyen <hoang24@pdx.edu>
2022-02-23 02:14:41 -0800

    update the template for Eyeudio GUI as discussed on 2/16/2022

--- gui.py
+++ gui.py
@@ -1,108 +1,61 @@
-'''
-    Usage: 
-        1. Install Kivy dependencies: pip install requirements.txt
-        2. Run GUI: python gui.py
-'''
-
-# General libraries
-import os
-
-# Kivy libraries
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.dropdown import DropDown
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scrollview import ScrollView
-from kivy.clock import Clock
-from kivy.uix.image import Image
-from kivy.core.window import Window
-from kivy.uix.textinput import TextInput
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.properties import ListProperty
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
-# Global variables
-TITLE_SIZE = 30
-BUTTON_FONT_SIZE = 20
-BUTTON_SIZE = (.35,.2)
-GREEN = (0,0.7,0,1)
-BLACK = (0,0,0,1)
+# Load template file
+Builder.load_file('template_gui.kv')
 
 # Kivy system variables
-Window.clearcolor = (1, 1, 1, 1) # Background Color (White)
-Window.size = (600, 400) # Window size (x, y)
+Window.size = (800, 500) # Window size (x, y)
 
 
-class ScreenManagement(ScreenManager):
-    '''
-        Initialization of the screen manager class to manage screens of GUI
-    '''
+class EyeudioGUI(Widget):
     def __init__(self, **kwargs):
-        super(ScreenManagement, self).__init__(**kwargs)
+        super(EyeudioGUI, self).__init__(**kwargs)
+        self.eye_on = False # Status of Eye Tracking       (ON/OFF)
+        self.lip_on = False # Status of Lip Reading        (ON/OFF)
+        self.aux_on = True  # Status of Speech Recognition (ON/OFF)
 
+    def _open_popup(self):
+        pop = Popup(title='Eyeudio',
+                    content=Label(text='More Info Here'),
+                    size_hint=(None, None), size=(400, 400))
+        pop.open()
 
-class MainMenu(Screen):
-    '''
-        Main Menu screen
-    '''
-    def __init__(self, **kwargs):
-        super(MainMenu, self).__init__(**kwargs)
-
-        # Image widget
-        self.img = Image(source='images/logo.png', pos_hint={'x':.75, 'center_y':.85}, size_hint=(.25, .25))
-        self.add_widget(self.img)
-
-        # Label widget
-        self.title = Label(text="Assistive Eyeudio Control GUI", font_size=TITLE_SIZE, color=BLACK, pos_hint={'x':.35, 'center_y':.85}, size_hint=(.125,.1))
-        self.add_widget(self.title)
-
-        # Button widget
-        self.button = Button(text="Starting Eyeudio", font_size=BUTTON_FONT_SIZE, background_color=GREEN, pos_hint={'x':0.35, 'center_y': .1}, size_hint=BUTTON_SIZE)
-        self.add_widget(self.button)
-        self.button.bind(on_release=self.start_eyeudio)
-
-    def start_eyeudio(self, *args):
-        self.manager.current = 'Eyeudio'
-
-
-class Eyeudio(Screen):
-    '''
-        Eyeudio screen
-    '''
-    def __init__(self, **kwargs):
-        super(Eyeudio, self).__init__(**kwargs)
-
-        # Label widget
-        self.title = Label(text="Start Listening", font_size=TITLE_SIZE, color=BLACK, pos_hint={'x':.35, 'center_y':.85}, size_hint=(.125,.1))
-        self.add_widget(self.title)
-
-        # Button widget
-        self.button = Button(text="Back", font_size=BUTTON_FONT_SIZE, background_color=GREEN, pos_hint={'x':0.35, 'center_y': .1}, size_hint=BUTTON_SIZE)
-        self.add_widget(self.button)
-        self.button.bind(on_release=self.go_back)
-
-    def go_back(self, *args):
-        self.manager.current = 'MainMenu'
+    def _update_button(self, event):
+        self.event = event
+        if self.event == "click_eye_btn":
+            if self.eye_on:
+                self.ids.eye_btn.text = "OFF"
+                self.eye_on = False
+            else:
+                self.ids.eye_btn.text = "ON"
+                self.eye_on = True
+        elif self.event == "click_lip_btn":
+            if self.lip_on:
+                self.ids.lip_btn.text = "OFF"
+                self.lip_on = False
+            else:
+                self.ids.lip_btn.text = "ON"
+                self.lip_on = True
+        elif self.event == "click_aux_btn":    
+            if self.aux_on:
+                self.ids.aux_btn.text = "OFF"
+                self.aux_on = False
+            else:
+                self.ids.aux_btn.text = "ON"
+                self.aux_on = True
 
 
 class Application(App):
-    '''
-        Build all the Kivy screens
-    '''
     def build(self):
-        '''
-            TODO: Add new screens here by sm.add_widget(NewScreenName(name='NewScreenName'))
-        '''
-        sm = ScreenManagement(transition=NoTransition())
-        sm.add_widget(MainMenu(name='MainMenu'))
-        sm.add_widget(Eyeudio(name='Eyeudio'))
-        return sm
+        return EyeudioGUI()
 
 
-# Run the Graphic User Interface program
 if __name__ == "__main__":
     Application().run()
